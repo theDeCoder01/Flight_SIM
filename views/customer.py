0c353db1c568e6c800a51c0fe79816f0691975d4
Ali Farrah <104384368+theDeCoder01@users.noreply.github.com>
2022-08-15 00:46:26 +0300

    Update customer.py

--- views/customer.py
+++ views/customer.py
@@ -169,14 +169,14 @@ def add_ticket():
         flight_id = request.args.get("flight_id")
         customer_id = get_customer_by_user_id(user_id =  customer_facade._user_data["id"]).id
         print("flight id: ", flight_id, customer_id)
-        #try:
-        customer_facade.add_ticket(
+        try:
+            customer_facade.add_ticket(
                 flight_id=flight_id, customer_id=customer_id
-            )
-        return redirect(url_for("customer_page.portal"))
-        #except:
-        #    ticket_error = "You already have a ticket for this flight"
-        #    return redirect(url_for("customer_page.portal", ticket_error=ticket_error))
+                )
+            return redirect(url_for("customer_page.portal"))
+        except:
+            ticket_error = "You already have a ticket for this flight"
+            return redirect(url_for("customer_page.portal", ticket_error=ticket_error))
     else:
         return redirect(
             url_for(
